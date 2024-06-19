9e69c45143c7b4f82293d7bc5e574a1dabade8d8
young0519 <kiny0606@naver.com>
2024-06-19 23:19:05 +0900

    Feat : 영수증 출력 제한 시간 설정 (#3)

--- src/components/receiptModal.js
+++ src/components/receiptModal.js
@@ -2,25 +2,41 @@ import { useDispatch } from "react-redux";
 import * as md from "../styles/modalStyle";
 import { SetReceiptModal } from "../redux/kioskAction";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const ReceiptModal = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [countdown, setCountdown] = useState(9);
+
+  const NoReceipt = () => {
+    dispatch(SetReceiptModal(false));
+    navigate('/');
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCount) => prevCount - 1);
+    }, 1000);
+
+    if (countdown === 0) {
+      NoReceipt();
+      clearInterval(timer);
+    }
+
+    return () => clearInterval(timer);
+  }, [countdown, NoReceipt]);
 
   const ReceiptPrint = () => {
     dispatch(SetReceiptModal(false));
     navigate('/');
-    }
-    const NoReceipt = () => {
-      dispatch(SetReceiptModal(false));
-      navigate('/');
   }
 
   return (
     <md.ReceiptModalContainer>
       <h1>[정상 승인]</h1>
       <h1>영수증을 출력하시겠습니까?</h1>
-      <h1>9 초 후 자동 미출력</h1>
+      <h1>{countdown} 초 후 자동 미출력</h1>
       <div className="btnBox">
         <button 
           onClick={ReceiptPrint}
